528836c7fdc07b6a97094d2ac3827d98042f7dda
DmitryZalogin <zalogindmitric@gmail.com>
2024-10-03 23:24:55 +0300

    Python

---
+++ 3.1.1.py
@@ -0,0 +1,10 @@
+a=int(input('Введите число 1 '))
+b=int(input('Введите число 2 '))
+c=int(input('Введите число 3 '))
+d=[1,2,3]
+if a in d:
+  print(a)
+if b in d:
+   print(b)
+if c in d:
+   print(c)
